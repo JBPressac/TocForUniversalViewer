cff4b13f184262af79ac2c910bd07d4584ea69c2
Jean-Baptiste Pressac <jb_pressac@yahoo.fr>
2018-05-22 15:05:31 +0200

    Essai d'ajout d'une structure d'essai au manifeste des items.

---
+++ TocForUniversalViewerPlugin.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * Table of content for Universal Viewer
+ *
+ *
+ * @copyright Jean-Baptiste Pressac, 2018
+ * @license https://www.cecill.info/licences/Licence_CeCILL_V2.1-en.html
+ *  */
+
+/**
+ * The Table of content for UniversalViewer plugin.
+ * @package Omeka\Plugins\TocForUniversalViewer
+ */
+class TocForUniversalViewerPlugin extends Omeka_Plugin_AbstractPlugin
+{
+    protected $_hooks = array(
+      'public_items_show',
+    );
+
+    protected $_filters = array(
+      'uv_manifest',
+    );
+
+    /**
+     * Print out Hello on item show
+     */
+    public function hookPublicItemsShow()
+    {
+           echo '<h1>Hello World</h1>';
+    }
+
+    public function filterUvManifest($manifest, $args)
+    {
+      // echo '<p>' + $manifest + '</p>';
+      // $structures = '"structures":[{"@id":"https://wellcomelibrary.org/iiif/b18035723/range/r-0","@type":"sc:Range","label":"Front Cover","canvases":["https://wellcomelibrary.org/iiif/b18035723/canvas/c0"]},{"@id":"https://wellcomelibrary.org/iiif/b18035723/range/r-2","@type":"sc:Range","label":"Title Page","canvases":["https://wellcomelibrary.org/iiif/b18035723/canvas/c3"]},{"@id":"https://wellcomelibrary.org/iiif/b18035723/range/r-1","@type":"sc:Range","label":"Back Cover","canvases":["https://wellcomelibrary.org/iiif/b18035723/canvas/c1"]}]'
+      $manifest['label'] = "Label bleu" ;
+      $manifest['structures'] = array(array('@id' => "https://wellcomelibrary.org/iiif/b18035723/range/r-0",
+      '@type' => "sc:Range", 'label' => "Front Cover", 'canvases' => "https://wellcomelibrary.org/iiif/b18035723/canvas/c0"),
+      array('@id' => "https://wellcomelibrary.org/iiif/b18035723/range/r-1"));
+
+      // $manifest = $manifest;
+      return $manifest ;
+    }
+}
